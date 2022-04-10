cc2b85ad7708a875114c2265b273fdde8ad00096
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2022-04-11 01:06:26 +0300

    mobile styling

--- app/Views/template.php
+++ app/Views/template.php
@@ -4,12 +4,16 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
         <script src="https://kit.fontawesome.com/d061b336ad.js" crossorigin="anonymous"></script>
-        <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>/css/styles.css">     
+        <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>/css/styles.css">    
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     </head>
         <body>
             <header>
                 <div class="logo">Twitt</div>
                 <div class="header-actions">
+                <i class="fa-solid fa-bars menu-toggle"></i>
+                <div class="menu">
                     <a href="<?= base_url() ?>/twitt/post" class="primary-button">Post</a>
                     <a href="<?= base_url() ?>/home">
                         <i class="fa-solid fa-house-chimney-window"></i>   
@@ -19,7 +23,13 @@
                     </a>
                     <a href="<?= base_url() ?>/account/logout" class="secondary-button">Logout</a>
                 </div>
+                </div>
             </header>
             <?= view($body);?>
+            <script>
+                $('.menu-toggle').on('click', function() {
+                    $('.menu').toggleClass('open');
+                });
+            </script>
         </body>
 </html>
