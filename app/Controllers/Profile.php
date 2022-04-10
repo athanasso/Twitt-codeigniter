a71022a656ba4fbe25494b7b58965e9939dde470
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2022-04-10 20:44:50 +0300

    profile view creation util

---
+++ app/Controllers/Profile.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\HTTP\RequestInterface;
+
+class Profile extends BaseController
+{
+    public function viewProfile()
+    {
+        $userId = $this->request->getVar('id');
+
+        $userModel = new \App\Models\User();
+
+        $user = $userModel->where('id', $userId)->first();
+
+        $twittModel = new \App\Models\Twitt();
+
+        $twitts = $twittModel->where('user_id', $userId)->findAll();
+
+        return view('profile', ['user' => $user, 'twitts' => $twitts]);
+    }
+}
--- app/Views/profile.php
+++ app/Views/profile.php
@@ -22,25 +22,19 @@
         <main class="profile-main">
             <div class="profile-view">
                 <i class="fa-solid fa-circle-user user-profile-pic"></i>
-                <a href="#" class="primary-button">Follow</a>
+                <h1><?= $user['username'] ?></h1>
+                <a href="<? base_url() ?>/follow/addFollow?user=<? $user['id'] ?>" class="primary-button">Follow</a>
                 <div class="twitts-feed">
+                    <?php foreach ($twitts as $twitt): ?>
                     <div class="twitt">
                         <div class="profile-pic">
                             <i class="fa-solid fa-circle-user profile-pic"></i>
                         </div>
                         <div class="message">
-                            <p>Hello, this is my first twitt!</p>
-                            <p class="metadata">Posted on 20/1/2022</p>
+                            <p><?= $twitt->body ?></p>
+                            <p class="metadata">Posted on <?= $twitt->created_at ?></p>
                         </div>
-                    </div>
-                    <div class="twitt">
-                        <div class="profile-pic">
-                            <i class="fa-solid fa-circle-user profile-pic"></i>
-                        </div>
-                        <div class="message">
-                            <p>Hello, this is my second twitt!</p>
-                            <p class="metadata">Posted on 20/1/2022</p>
-                        </div>
+                        <?php endforeach; ?>
                     </div>
                 </div>
             </div>
